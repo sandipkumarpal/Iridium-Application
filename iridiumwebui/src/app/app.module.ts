08d7f70f30f0adc3715f5f98fafe43e9320ee1ca
sandip <dasa@gmail.com>
2018-08-10 17:33:38 +0530

    Adds Header & Angular Router

--- iridiumwebui/src/app/app.module.ts
+++ iridiumwebui/src/app/app.module.ts
@@ -5,17 +5,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoaderComponent } from './loader/loader.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoaderComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path: '', component: LoaderComponent }
+      { path: '', component: HomeComponent },
+      { path: 'loader', component: LoaderComponent },
+      { path: '**', component: LoaderComponent }
     ]),
   ],
   entryComponents: [
